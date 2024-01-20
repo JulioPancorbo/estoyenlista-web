116524c8ce2b58cf469b5ab8ed85647dfaf61d4f
JulioPancorbo <juliopancorbo@xerintel.es>
2024-01-20 16:27:21 +0100

    FIrst commit with configurations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAiBnL3aLDFcYBddW3MYhOachqUq71Cu1A",
+    authDomain: "listapp-c1115.firebaseapp.com",
+    projectId: "listapp-c1115",
+    storageBucket: "listapp-c1115.appspot.com",
+    messagingSenderId: "986664709457",
+    appId: "1:986664709457:web:5a658a024d11e7f7e57770",
+    measurementId: "G-M9H9PYXW71"
+  }
 };
 
 /*
