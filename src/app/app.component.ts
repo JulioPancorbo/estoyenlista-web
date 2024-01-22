db6d05c4751eeb88af96c18dab1674b77ffd908b
JulioPancorbo <juliopancorbo@xerintel.es>
2024-01-22 18:04:20 +0100

    Hide or show menu when logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  user: any;
+
+  constructor(
+    private navCtrl: NavController,
+    private menu: MenuController,
+    private router: Router
+  ) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        if (event.urlAfterRedirects === '/login') {
+          this.menu.enable(false);
+        } else {
+          this.menu.enable(true);
+        }
+      });
+  }
+
+  navigateTo(page: string) {
+    this.navCtrl.navigateForward('/' + page);
+  }
 }
